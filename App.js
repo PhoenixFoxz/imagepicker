af89e3ba9398f0398c74691a57b90fef7f6b7dfc
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-03-22 09:25:50 -0300

    Função escolherFoto

--- App.js
+++ App.js
@@ -12,19 +12,37 @@ export default function App() {
 
   console.log(status);
 
+  /* Ao entrar no app, será executada a verificação de permissões de uso */
   useEffect(() => {
+    /* Esta função mostrará um popup para o usuário perguntando se ele autoriza a utilização do recurso móvel (no caso, selecionar/tirar foto). */
     async function verificaPermissoes() {
       const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
+
+      /* Ele dando autorização (granted), isso será armazenado no state de requestPermission */
       requestPermission(cameraStatus === "granted");
     }
     verificaPermissoes();
   }, []);
 
+  const escolherFoto = async () => {
+    const resultado = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 1,
+    });
+
+    if (!resultado.canceled) {
+      setFoto(resultado.assets[0].uri);
+    }
+  };
+  console.log(foto);
+
   return (
     <>
       <StatusBar style="auto" />
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Button title="Escolher foto" />
+        <Button title="Escolher foto" onPress={escolherFoto} />
         <Image style={{ width: 300, height: 300 }} />
       </View>
     </>
